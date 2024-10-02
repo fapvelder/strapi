6b314483655a45aca373bf57ac98e08728308837
fapvelder <lyhungphat25@gmail.com>
2024-10-02 10:30:30 +0700

    first commit

---
+++ src/api/fdg/controllers/fdg.ts
@@ -0,0 +1,7 @@
+/**
+ * fdg controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::fdg.fdg');
---
+++ src/api/fdg/routes/fdg.ts
@@ -0,0 +1,7 @@
+/**
+ * fdg router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::fdg.fdg');
